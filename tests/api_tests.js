1fe71838483c1208b5f11e6b4e546953c1d3d995
VadimNastoyashchy <vadym_nastoiashchyi@epam.com>
2024-01-17 21:20:30 +0200

    Add tests

---
+++ tests/api_tests.js
@@ -0,0 +1,11 @@
+import { describe, test, expect } from "real-test-js";
+import axios from "axios";
+
+describe('Api tests for SWAPI', () => {
+  test('Get Luke Skywalker', async () => {
+    const response = await axios.get(`https://swapi.dev/api/people/1/`);
+    expect(response.data.name).toBeEqual('Luke Skywalker')
+    expect(response.data.height).toBeNotNull()
+    expect(response.data.films).toHaveLength(4)
+  });
+});
--- tests/test.js
+++
@@ -1,8 +0,0 @@
-import { describe, test, expect} from 'real-test-js'
-
-describe('Title for describe block', () => {
-  test('Title for test', () => {
-    const arr = [1, 2, 3]
-    expect(arr).toHaveLength(3)
-  })
-})
