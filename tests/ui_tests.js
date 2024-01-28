aa93b2b881460970754e025e70888b26ea868eb0
VadimNastoyashchy <Vadim.89@ukr.net>
2024-01-28 09:22:29 +0200

    Update test to check async hooks

--- tests/ui_tests.js
+++ tests/ui_tests.js
@@ -1,19 +1,35 @@
-import { describe, test, expect } from "real-test-js";
+import { describe, test, expect, beforeEach, afterEach } from "real-test-js";
 import { chromium, devices } from "playwright";
 
 describe("UI tests using playwright", () => {
-  test("title", { timeout: 20_000 }, async () => {
-    const browser = await chromium.launch({ headless: true });
-    const context = await browser.newContext(devices["iPhone 11"]);
-    const page = await context.newPage();
+  let browser;
+  let context;
+  let page;
 
+  beforeEach(async () => {
+    browser = await chromium.launch({ headless: true });
+    context = await browser.newContext(devices["iPhone 11"]);
+    page = await context.newPage();
+  });
+
+  afterEach(async () => {
+    await context.close();
+    await browser.close();
+  });
+
+  test("title", { timeout: 20_000 }, async () => {
     await context.route("**.jpg", (route) => route.abort());
     await page.goto("https://playwright.dev/");
 
-    const title = await page.locator('.getStarted_Sjon').textContent()
+    const title = await page.locator(".getStarted_Sjon").textContent();
     expect(title).toBeEqual("Get started");
-   
-    await context.close();
-    await browser.close();
+  });
+
+  test("title2", { timeout: 20_000 }, async () => {
+    await context.route("**.jpg", (route) => route.abort());
+    await page.goto("https://playwright.dev/");
+
+    const title = await page.locator(".getStarted_Sjon").textContent();
+    expect(title).toBeEqual("Get started");
   });
 });
